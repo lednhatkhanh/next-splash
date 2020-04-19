e0b977c52e91711e894f3f2db04576ab1d325fa2
Khanh Le <lednhatkhanh@gmail.com>
2020-04-19 14:23:00 +0700

    fix: Pass req along to fetchUnsplash

--- src/pages/api/photos/[id]/download.js
+++ src/pages/api/photos/[id]/download.js
@@ -1,7 +1,9 @@
 import { fetchUnsplash } from "~/utils";
 
 const trackPhotoDownloadAPI = async (req, res) => {
-  const result = await fetchUnsplash(`photos/${req.query.id}/download`);
+  const result = await fetchUnsplash(`photos/${req.query.id}/download`, {
+    req,
+  });
   res.send(result);
 };
 
